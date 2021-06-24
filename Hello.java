35974e33d82d9374409c1dac8fe4092255bca4f8
apdevops <ashwini.ganjewar@gmail.com>
2021-06-24 16:15:13 +0000

    testing branching

--- Hello.java
+++ Hello.java
@@ -4,6 +4,8 @@
 class Hello{
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
-        System.out.println("Simple Git Testing....:"); 
+        System.out.println("Simple Git Testing....:");
+        System.out.println("New statement added in branch_1");
+	System.out.println("testing branching");
     }
 }
